abbd2825a5d084dae4d096e76e499d0704b733bb
saad <saadeld875@gmail.com>
2025-11-27 17:40:30 +0100

    first commit

---
+++ backend/src/main/java/com/example/backend_syndic/BackendSyndicApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_syndic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSyndicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSyndicApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/backend_syndic/BackendSyndicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_syndic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSyndicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
