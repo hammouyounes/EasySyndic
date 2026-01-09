f8a9a6ce3adeadf8f135b7e6ed2df84599f9b818
youness <younesshamou013@gmail.com>
2026-01-09 02:13:45 +0100

    fix : fixing data tables error

--- Front-end/src/pages/Users/UserList.tsx
+++ Front-end/src/pages/Users/UserList.tsx
@@ -42,7 +42,26 @@ const UserList: React.FC = () => {
         const timer = setTimeout(() => {
            dataTableInstance.current = new DataTable(tableRef.current!, {
               language: {
-                  url: '//cdn.datatables.net/plug-ins/1.13.3/i18n/fr-FR.json'
+                  processing:     "Traitement en cours...",
+                  search:         "Rechercher&nbsp;:",
+                  lengthMenu:    "Afficher _MENU_ &eacute;l&eacute;ments",
+                  info:           "Affichage de l'&eacute;lement _START_ &agrave; _END_ sur _TOTAL_ &eacute;l&eacute;ments",
+                  infoEmpty:      "Affichage de l'&eacute;lement 0 &agrave; 0 sur 0 &eacute;l&eacute;ments",
+                  infoFiltered:   "(filtr&eacute; de _MAX_ &eacute;l&eacute;ments au total)",
+                  infoPostFix:    "",
+                  loadingRecords: "Chargement en cours...",
+                  zeroRecords:    "Aucun &eacute;l&eacute;ment &agrave; afficher",
+                  emptyTable:     "Aucune donnée disponible dans le tableau",
+                  paginate: {
+                      first:      "Premier",
+                      previous:   "Pr&eacute;c&eacute;dent",
+                      next:       "Suivant",
+                      last:       "Dernier"
+                  },
+                  aria: {
+                      sortAscending:  ": activer pour trier la colonne par ordre croissant",
+                      sortDescending: ": activer pour trier la colonne par ordre décroissant"
+                  }
               },
               destroy: true,
               autoWidth: false,
